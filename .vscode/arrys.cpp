195d92095c1e09956302afaa42183980984467bf
duvan <you@example.com>
2023-06-03 12:28:19 -0500

    hola

---
+++ .vscode/arrys.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+Using namespace std;
+Int main (){
+Int mi_array[]{10,20,30,40,50,60}
+Cout<<mi_array{2}<<end1;
+Cout<<mi_array{4};
+}
---
+++ c++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    const float precio_opcion_1 =2,14;
+    const float precio_opcion_2 =4,80;
+    const float igv =0,18;
+    const int dias_vigencia =45;
+
+    int opcion;
+    float preciopormetro, preciosinigv, precioconigv, preciototal;
+
+    cout << "seleccione la opcion deseada; \n";
+    cont << "1,precio por metro de s/2,14 \n";
+    cout << "2,precio por metro de s/4.80 \n";
+    cin >> opcion;
+
+
+}
